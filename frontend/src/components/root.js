8309b107fb399966a3fca6e45ec5254f0a02fbee
skumtong <sky_kumtong@yahoo.com>
2021-04-20 09:40:21 -0700

    Fix merge conflicts

--- frontend/src/components/root.js
+++ frontend/src/components/root.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import App from './app';
+import React from "react";
+import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
-
+import App from "./app";
 const Root = () => (
-  <HashRouter>
-    <App />
-  </HashRouter>
+	<Provider store={store}>
+		<HashRouter>
+			<App />
+		</HashRouter>
+	</Provider>
 );
-
 export default Root;
