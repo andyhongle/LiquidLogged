28f06493a12b4ec30deddfc3ba20f8a445d01eea
alexzhou415 <zhoualex10@yahoo.com>
2021-04-21 00:13:59 -0700

    Install chart.js

--- frontend/src/components/profile/profile_donut.js
+++ frontend/src/components/profile/profile_donut.js
@@ -2,7 +2,11 @@ import React from 'react';
 function ProfileDonut(props) {
     console.log(props);
     return(
-        <div>{props.filteredLiquids}</div>
+        <div>
+            <script>
+                
+            </script>
+        </div>
     )
 }
 
