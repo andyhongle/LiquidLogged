56c22601dcbc4566df9a41aaa3bb6847e8c2196d
aladdin731 <aladdin731@hotmail.com>
2021-04-21 01:17:50 -0700

    add goal

--- frontend/src/components/liquid/cup.jsx
+++ frontend/src/components/liquid/cup.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 
-export default function Cup({dailyAmount}) {
+export default function Cup({dailyAmount, goal}) {
     let message = 
-        dailyAmount < 500 ? "Come on! Water is the source of life" :  
-                            (dailyAmount < 1000 ? "Awesome! Almost Halfway!" : 
-                            (dailyAmount < 1500 ? "Excellent! Get more!" : 
-                            (dailyAmount < 2000 ? "Almost! Amost!" : 
-                            (dailyAmount === 2000 ? "Achieved!!!" : "Ohh~ That's too much!"))) )
+        dailyAmount < goal / 4 ? "Come on! Water is the source of life" :  
+                            (dailyAmount < goal / 2 ? "Awesome! Almost Halfway!" : 
+                            (dailyAmount < goal / 4 * 3 ? "Excellent! Get more!" : 
+                            (dailyAmount < goal ? "Almost! Amost!" : 
+                            (dailyAmount === goal ? "Achieved!!!" : "Ohh~ That's too much!"))) )
     return (
+        
         <div className="cup-section">
         <div className="message">{message}</div>
             <div className="cup">
-                 <div style={{height: dailyAmount / 2000 * 300}} className="level"></div>
+                 <div style={{height: dailyAmount / goal * 300}} className="level"></div>
                  <div className="straw"></div>
                  <div className="straw-head"> </div>
             </div>
