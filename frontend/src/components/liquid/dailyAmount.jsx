0a69a23ba920fe08b8b473daaeac5a1a5deb5351
aladdin731 <aladdin731@hotmail.com>
2021-04-20 23:51:32 -0700

    change css

--- frontend/src/components/liquid/dailyAmount.jsx
+++ frontend/src/components/liquid/dailyAmount.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function DailyAmount({dailyAmount}) {
     return (
         <div className="daily-amount-section">
-            <h1 className="daily-amount-title">Daily Tracker</h1>
+            <h1 className="daily-amount-title">Daily Amount</h1>
             <h1 className="daily-amount">{dailyAmount}ml</h1>
         </div>
     )
