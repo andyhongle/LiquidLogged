68c2daa0935b5e1af78ab7bcfdfc5c2dea48fd87
AndyLe <andyhongles@gmail.com>
2021-05-07 15:54:03 -0700

    added favicon added confirm password validation

--- validation/register.js
+++ validation/register.js
@@ -6,6 +6,7 @@ module.exports = function validateRegisterInput(data) {
 
     data.username = validText(data.username) ? data.username : '';
     data.password = validText(data.password) ? data.password : '';
+    data.password2 = validText(data.password2) ? data.password2 : '';
 
     if (!Validator.isLength(data.username, { min: 2, max: 30 })) {
         errors.username = 'Username must be between 2 and 30 characters';
@@ -21,6 +22,14 @@ module.exports = function validateRegisterInput(data) {
 
     if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
         errors.password = 'Password must be at least 6 characters';
+    }
+
+    if (Validator.isEmpty(data.password2)) {
+        errors.password2 = 'Confirm password field is required'
+    }
+
+    if (!Validator.equals(data.password, data.password2)) {
+        errors.password2 = 'Passwords must match';
     }
 
     return {
