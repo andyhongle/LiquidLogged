c984958788f6720d303d7b0f565872dc4fd32a17
aladdin731 <aladdin731@hotmail.com>
2021-05-10 11:42:37 -0700

    delete all console.log

--- frontend/src/components/profile/profile_donut.jsx
+++ frontend/src/components/profile/profile_donut.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Doughnut } from "react-chartjs-2";
 
 const ProfileDonut = ({filteredLiquids}) => {
-    console.log(filteredLiquids);
+    // console.log(filteredLiquids);
     const liquids = {};
     if (filteredLiquids[0]){
         filteredLiquids.forEach((liquid) => {
